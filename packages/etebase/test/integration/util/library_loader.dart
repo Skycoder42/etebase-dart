7395e6ac99662f700844c9b787f16f53bf06c291
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-12-01 07:04:58 +0100

    ..

--- packages/etebase/test/integration/util/library_loader.dart
+++ packages/etebase/test/integration/util/library_loader.dart
@@ -5,6 +5,6 @@ DynamicLibrary loadLibEtebase() {
   if (Platform.isWindows) {
     return DynamicLibrary.open(r'C:\usr\lib\libetebase.dll');
   } else {
-    return DynamicLibrary.open('/usr/lib/libetebase.so.0');
+    return DynamicLibrary.open('/usr/lib/libetebase.so');
   }
 }
--- packages/etebase/tool/integration/build_unix.sh
+++ packages/etebase/tool/integration/build_unix.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# $1: patch file
 set -ex
 
-# setup rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-export PATH="$PATH:$HOME/.cargo/bin"
+PATCH_FILE=$1
 
 # build and install libetebase
 libetebaseDir="$RUNNER_TEMP/libetebase"
 git clone https://github.com/etesync/libetebase.git -b v0.5.3 "$libetebaseDir"
 cd "$libetebaseDir"
+
+if [ -n "$PATCH_FILE" ]; then
+  git apply "$PATCH_FILE"
+fi
+
 make
 sudo make install
