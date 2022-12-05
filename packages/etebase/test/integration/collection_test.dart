7e47407ac16b70a1b748e69e392bf48b058bbbf2
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-12-05 08:17:05 +0100

    WIP

--- packages/etebase/test/integration/collection_test.dart
+++ packages/etebase/test/integration/collection_test.dart
@@ -1,4 +1,6 @@
 // ignore: test_library_import
+import 'dart:typed_data';
+
 import 'package:etebase/etebase.dart';
 import 'package:test/test.dart';
 
@@ -6,6 +8,8 @@ import 'util/library_loader.dart';
 import 'util/server_controller.dart';
 
 void main() {
+  const testCollectionType = 'etebase-dart.collection_test';
+
   late final Uri serverUri;
   late final EtebaseCollectionManager collectionManager;
 
@@ -68,4 +72,43 @@ void main() {
     expect(await collectionMeta.getMtime(), isNull);
     expect(await collectionMeta.getColor(), isNull);
   });
+
+  test('can create and upload collections', () async {
+    final meta1 = await EtebaseItemMetadata.create();
+    addTearDown(meta1.dispose);
+    await meta1.setName('create-collection-test-1');
+    final collection1 = await collectionManager.create(
+      testCollectionType,
+      meta1,
+      Uint8List.fromList(List.generate(20, (index) => index + 10)),
+    );
+    addTearDown(collection1.dispose);
+    await collectionManager.upload(collection1);
+
+    final meta2 = await EtebaseItemMetadata.create();
+    addTearDown(meta2.dispose);
+    await meta2.setName('create-collection-test-2');
+    await meta2.setMtime(DateTime.now().subtract(const Duration(days: 10)));
+    final collection2 = await collectionManager.create(
+      testCollectionType,
+      meta2,
+      Uint8List(0),
+    );
+    addTearDown(collection2.dispose);
+    await collectionManager.upload(collection2);
+
+    final meta3 = await EtebaseItemMetadata.create();
+    addTearDown(meta3.dispose);
+    final collection3 = await collectionManager.create(
+      '$testCollectionType-other',
+      meta3,
+      Uint8List(0),
+    );
+    addTearDown(collection3.dispose);
+    await collectionManager.upload(collection3);
+  });
+
+  // test('can list and fetch collection', () async {
+  //   final collections = await collectionManager.list(testCollectionType);
+  // });
 }
