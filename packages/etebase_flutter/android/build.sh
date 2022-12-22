c76a301d6cf0532f1774fee6ad63d791d60783d1
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-12-22 20:42:29 +0100

    Get android builds to work

--- packages/etebase_flutter/android/build.sh
+++ packages/etebase_flutter/android/build.sh
@@ -3,34 +3,42 @@ set -ex
 
 version=${1:-v0.5.3}
 install_dir=${2:-$PWD}
+host_arch=${3:-linux-x86_64}
+ndk_version=${4:-22.1.7171670}
 
-armv7="armeabi-v7a armv7-linux-androideabi armv7a-linux-androideabi19-clang"
-aarch64="arm64-v8a aarch64-linux-android aarch64-linux-android21-clang"
-x86="x86 i686-linux-android i686-linux-android19-clang"
-x86_64="x86_64 x86_64-linux-android x86_64-linux-android21-clang"
+armv7="armeabi-v7a armv7-linux-androideabi armv7a-linux-androideabi19-clang armv7a-linux-androideabi19-clang++"
+aarch64="arm64-v8a aarch64-linux-android aarch64-linux-android21-clang aarch64-linux-android21-clang++"
+x86="x86 i686-linux-android i686-linux-android19-clang i686-linux-android19-clang++"
+x86_64="x86_64 x86_64-linux-android x86_64-linux-android21-clang x86_64-linux-android21-clang"
 arches=( "$aarch64" "$x86_64" "$armv7" "$x86" )
 
 build_dir=$(mktemp -d)
 pushd "$build_dir"
 
 git clone https://github.com/etesync/libetebase -b "$version" .
-echo 'openssl = { version = "0.10.38", features = ["vendored"] }' >> Cargo.toml
 
+openssl_version=$(cat Cargo.lock | grep 'name = "openssl"' -A 3 | grep "version" | cut -d '"' -f 2)
+echo "openssl = { version = \"$openssl_version\", features = [\"vendored\"] }" >> Cargo.toml
+
+export PATH="$ANDROID_HOME/ndk/$ndk_version/toolchains/llvm/prebuilt/$host_arch/bin/:$PATH"
 for arch in "${arches[@]}"; do
-    arch=($arch)
-    jniLibArch=${arch[0]}
-    target=${arch[1]}
-    ccompiler=${arch[2]}
+  cargo clean
 
-    jniDir="$install_dir/src/main/jniLibs/$jniLibArch"
+  arch=($arch)
+  jniLibArch=${arch[0]}
+  target=${arch[1]}
+  export CC=${arch[2]}
+  export CXX=${arch[3]}
 
-    export CC="$ccompiler"
-    export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ccompiler"
+  jniDir="$install_dir/src/main/jniLibs/$jniLibArch"
 
-    cargo build --target "$target" --release
+  cargo build \
+    --config "target.$target.linker=\"$install_dir/my-hacky-linker-cc\"" \
+    --target "$target" \
+    --release
 
-    mkdir -p "$jniDir"
-    cp "target/$target/release/libetebase.so" "$jniDir"
+  mkdir -p "$jniDir"
+  cp "target/$target/release/libetebase.so" "$jniDir"
 done
 
 popd
