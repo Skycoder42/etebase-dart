a1627b393cbc06a53d7ad2a4ed53ab0387d2eb02
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-12-28 15:10:58 +0100

    ...

--- packages/etebase_flutter/tool/libetebase/android.sh
+++ packages/etebase_flutter/tool/libetebase/android.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -ex
 
-version=${1:-v0.5.3}
+version=${1:?First argument must be the libetebase version to build}
 install_dir=${2:-$PWD}
 host_arch=${3:-linux-x86_64}
 ndk_version=${4:-22.1.7171670}
-jniDir="$install_dir/android/src/main/jniLibs"
-cacheDir="$install_dir/tool/libetebase/lib"
+jni_dir="$install_dir/android/src/main/jniLibs"
+cache_dir="$install_dir/tool/libetebase/lib"
 
 if [ "$CACHE_HIT" = "true" ]; then
-  cp -a "$cacheDir" "$jniDir"
+  cp -a "$cache_dir" "$jni_dir"
+  exit 0
 fi
 
 armv7="armeabi-v7a armv7-linux-androideabi armv7a-linux-androideabi19-clang armv7a-linux-androideabi19-clang++"
@@ -18,10 +19,9 @@ x86="x86 i686-linux-android i686-linux-android19-clang i686-linux-android19-clan
 x86_64="x86_64 x86_64-linux-android x86_64-linux-android21-clang x86_64-linux-android21-clang"
 arches=( "$aarch64" "$x86_64" "$armv7" "$x86" )
 
-build_dir=$(mktemp -d)
-pushd "$build_dir"
-
-git clone https://github.com/etesync/libetebase -b "$version" .
+build_dir=$RUNNER_TEMP/libetebase
+git clone https://github.com/etesync/libetebase -b "$version" "$build_dir"
+cd "$build_dir"
 
 openssl_version=$(cat Cargo.lock | grep 'name = "openssl"' -A 3 | grep "version" | cut -d '"' -f 2)
 echo "openssl = { version = \"$openssl_version\", features = [\"vendored\"] }" >> Cargo.toml
@@ -31,7 +31,7 @@ for arch in "${arches[@]}"; do
   cargo clean
 
   arch=($arch)
-  jniLibArch=${arch[0]}
+  jni_lib_arch=${arch[0]}
   target=${arch[1]}
   export CC=${arch[2]}
   export CXX=${arch[3]}
@@ -42,12 +42,9 @@ for arch in "${arches[@]}"; do
     --target "$target" \
     --release
 
-  jniArchDir="$jniDir/$jniLibArch"
-  mkdir -p "$jniArchDir"
-  cp -a "target/$target/release/libetebase.so" "$jniArchDir"
+  jni_arch_dir="$jni_dir/$jni_lib_arch"
+  mkdir -p "$jni_arch_dir"
+  cp -a "target/$target/release/libetebase.so" "$jni_arch_dir"
 done
 
-popd
-rm -rf "$build_dir"
-
-cp -a "$jniDir" "$cacheDir"
+cp -a "$jni_dir" "$cache_dir"
---
+++ packages/etebase_flutter/tool/libetebase/linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#$1 libetebase version
+set -ex
+
+version=${1:?First argument must be the libetebase version to build}
+install_dir=${2:-$PWD}
+cache_dir="$install_dir/tool/libetebase/lib"
+
+if [ "$CACHE_HIT" = "true" ]; then
+  sudo rsync -a "$cache_dir/" /
+  exit 0
+fi
+
+# build and install libetebase
+build_dir="$RUNNER_TEMP/libetebase"
+git clone https://github.com/etesync/libetebase.git -b "$version" "$build_dir"
+cd "$build_dir"
+make
+make install DESTDIR="$cache_dir"
+
+sudo rsync -a "$cache_dir/" /
