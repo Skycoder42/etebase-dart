8cc755e37067a36081ac1e44b5024f35ff1c1296
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-12-31 15:46:05 +0100

    ......

---
+++ packages/etebase_flutter/tool/libetebase/windows.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$1 libetebase version
+set -ex
+
+version=${1:?First argument must be the libetebase version to build}
+
+lib_dir="$PWD/windows/lib"
+cache_dir="$GITHUB_WORKSPACE/.cache"
+
+if [ "$CACHE_HIT" = "true" ]; then
+  mkdir -p "$lib_dir"
+  rsync -av "$cache_dir/" "$lib_dir/"
+  exit 0
+fi
+
+# build and install libetebase
+build_dir="$RUNNER_TEMP/libetebase"
+git clone https://github.com/etesync/libetebase.git -b "v$version" "$build_dir"
+pushd "$build_dir"
+
+cargo build --release
+
+mkdir -p "$cache_dir"
+mv target/release/etebase.dll "$cache_dir/"
+
+mkdir -p "$lib_dir"
+rsync -av "$cache_dir/" "$lib_dir/"
