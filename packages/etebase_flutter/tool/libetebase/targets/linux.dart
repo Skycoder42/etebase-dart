b6b3794e386e4ae4b863930a0b19ce76f80f27ba
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-07-26 06:12:53 +0000

    ....

--- packages/etebase_flutter/tool/libetebase/targets/linux.dart
+++ packages/etebase_flutter/tool/libetebase/targets/linux.dart
@@ -6,23 +6,28 @@ import 'build_platform.dart';
 import 'build_target.dart';
 
 final class LinuxTarget extends BuildTarget {
-  static const aarch64 = LinuxTarget._(
-    architecture: 'aarch64',
-    rustTarget: 'aarch64-unknown-linux-gnu',
-  );
   static const x86_64 = LinuxTarget._(
     architecture: 'x86_64',
     rustTarget: 'x86_64-unknown-linux-gnu',
   );
 
+  static const aarch64 = LinuxTarget._(
+    architecture: 'aarch64',
+    rustTarget: 'aarch64-unknown-linux-gnu',
+    useCross: true,
+  );
+
   final String _architecture;
   @override
   final String rustTarget;
+  final bool _useCross;
 
   const LinuxTarget._({
     required String architecture,
     required this.rustTarget,
-  }) : _architecture = architecture;
+    bool useCross = false,
+  })  : _architecture = architecture,
+        _useCross = useCross;
 
   @override
   String get name => 'linux_$_architecture';
@@ -31,15 +36,17 @@ final class LinuxTarget extends BuildTarget {
   String get binaryName => 'libetebase.so';
 
   @override
-  String get buildTool => 'cross';
+  String get buildTool => _useCross ? 'cross' : super.buildTool;
 
   @override
   Future<void> fixupSources(Directory srcDir) async {
-    await Github.exec(
-      'cargo',
-      const ['install', '-f', 'cross'],
-      workingDirectory: srcDir,
-    );
+    if (_useCross) {
+      await Github.exec(
+        'cargo',
+        const ['install', '-f', 'cross'],
+        workingDirectory: srcDir,
+      );
+    }
   }
 }
 
