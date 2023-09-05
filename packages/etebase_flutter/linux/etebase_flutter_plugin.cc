527aa683901b07b45c544485edaa785f247793eb
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-09-05 08:32:41 +0200

    update native projects

--- packages/etebase_flutter/linux/etebase_flutter_plugin.cc
+++ packages/etebase_flutter/linux/etebase_flutter_plugin.cc
@@ -1,3 +1,30 @@
 #include "include/etebase_flutter/etebase_flutter_plugin.h"
 
-void etebase_flutter_plugin_register_with_registrar(FlPluginRegistrar* registrar) {}
+#include <flutter_linux/flutter_linux.h>
+#include <gtk/gtk.h>
+
+#define ETEBASE_FLUTTER_PLUGIN(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), etebase_flutter_plugin_get_type(), \
+                              EtebaseFlutterPlugin))
+
+struct _EtebaseFlutterPlugin {
+  GObject parent_instance;
+};
+
+G_DEFINE_TYPE(EtebaseFlutterPlugin, etebase_flutter_plugin, g_object_get_type())
+
+static void etebase_flutter_plugin_dispose(GObject* object) {
+  G_OBJECT_CLASS(etebase_flutter_plugin_parent_class)->dispose(object);
+}
+
+static void etebase_flutter_plugin_class_init(EtebaseFlutterPluginClass* klass) {
+  G_OBJECT_CLASS(klass)->dispose = etebase_flutter_plugin_dispose;
+}
+
+static void etebase_flutter_plugin_init(EtebaseFlutterPlugin* self) {}
+
+void etebase_flutter_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
+  EtebaseFlutterPlugin* plugin = ETEBASE_FLUTTER_PLUGIN(
+      g_object_new(etebase_flutter_plugin_get_type(), nullptr));
+  g_object_unref(plugin);
+}
